62ca65258baec37ecff3615d934952f24630f80a
Eduardo Botelho <eduardo11224b@gmail.com>
2025-09-17 13:27:17 +0100

    initial commit

---
+++ src/main/java/com/upsxace/tv_show_tracker/TvShowTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.upsxace.tv_show_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvShowTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvShowTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upsxace/tv_show_tracker/TvShowTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upsxace.tv_show_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvShowTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
